3f4c9b9a6facf322e5cbf417f754ebd395ecbef1
D <D>
2016-12-04 22:47:10 +0700

    wday

---
+++ conn.php
@@ -0,0 +1,9 @@
+<?php
+//error_reporting(0);
+$con=mysqli_connect("localhost","root","","db_sman2");
+//	$conn=mysqli_connect('localhost','root','');
+//	mysqli_select_db($conn, 'db_sman2');
+//$db = new PDO('mysql:host=localhost;dbname=db_sman2;charset=utf8mb4', 'root', '');
+//$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+?>
